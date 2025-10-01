107bbef87e57110d0d67beaab104d03a78c95a81
Sakoooooo <78461130+Sakooooo@users.noreply.github.com>
2025-10-01 14:45:33 +0300

    add FolderNotFound and TaskNotFound to errors

--- src/helpers/errors.rs
+++ src/helpers/errors.rs
@@ -1,12 +1,16 @@
 #[derive(Debug)]
 pub enum CommonErrors {
     NoFolders,
+    FolderNotFound,
+    TaskNotFound,
 }
 
 impl std::fmt::Display for CommonErrors {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::NoFolders => write!(f, "You have no folders!"),
+            Self::TaskNotFound => write!(f, "Could not find task!"),
+            Self::FolderNotFound => write!(f, "Could not find folder!"),
         }
     }
 }
